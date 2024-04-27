1add8376ab8123e94058b66f0718c87e1f7d98fa
ahmetilhn <ahmetilhan.dev@gmail.com>
2024-04-27 15:38:24 +0300

    added base structure

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+import path from "path";
+const nodeExternals = require("webpack-node-externals");
+module.exports = {
+  mode: "production",
+  target: "web",
+  entry: "./lib/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: [/node_modules/, /demo/, /build/],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  externals: [nodeExternals()],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+    library: {
+      name: "ErrorGuardian",
+      type: "umd",
+      export: "default",
+      umdNamedDefine: true,
+    },
+    globalObject: "this",
+  },
+};
